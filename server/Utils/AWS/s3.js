7f43c9799ce27655cbf5648888779322383b1ea8
reddy-nithin <reddy.nithin.0331@gmail.com>
2021-10-20 21:37:44 +0530

    Project Completion

---
+++ server/Utils/AWS/s3.js
@@ -0,0 +1,16 @@
+import AWS from "aws-sdk";
+
+const s3Bucket = new AWS.S3({
+  accessKeyId: "AKIA3UN2SDLPHZ2T3OP5",
+  secretAccessKey: "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",
+  region: "ap-south-1"
+});
+
+export const s3Upload = (options) => {
+  return new Promise((resolve, reject)=>
+    s3Bucket.upload(options, (error,data)=>{
+      if(error) return reject(error);
+      return resolve(data);
+    })
+  );
+};
